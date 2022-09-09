3365a7f846b0266f207c442e5ac9586b0e94753e
ronan-ferreira <ronanifto@gmail.com>
2022-09-09 11:37:19 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
 
-
+import NotificationButton from "./componets/NotificationButton"
 function App() {
     return (
-    <h1>Olá mundo</h1>
+    <>
+        <h1>Olá mundo</h1>
+        <NotificationButton/>
+        <NotificationButton/>
+        <NotificationButton/>
+    </>
     )
 }
 
