907bc24e0d6a68fd0484895a1a25ebc5c2705c11
ronan-ferreira <ronanifto@gmail.com>
2022-09-11 21:12:23 -0300

    Conserto Date

--- frontend/src/componets/SalesCard/index.tsx
+++ frontend/src/componets/SalesCard/index.tsx
@@ -9,6 +9,8 @@ import { Sale } from "../../models/sale";
 
 
 function SalesCard() {
+  const max = new Date();
+  const min = new Date(new Date().setDate(new Date().getDate() - 365));
 
   const [minDate, setMinDate] = useState(new Date());
   const [maxDate, setMaxDate] = useState(new Date());
