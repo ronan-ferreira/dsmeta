06caaebdb67a1464e73e41ac9cfebcb850f32971
ronan-ferreira <ronanifto@gmail.com>
2022-09-11 21:26:44 -0300

    alteracão Desenvolvedor

--- frontend/src/componets/Header/index.tsx
+++ frontend/src/componets/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
                <h1>DSMeta</h1>
                <p>
                  Desenvolvido por
-                 <a href="https://github.com/ronan-ferreira/dsmeta">@ronanferreira</a>
+                 <a href="https://github.com/ronan-ferreira">ronanferreira</a>
                </p>
            </div>
        </header>
